7b304e30df274da43e59b369c330f3f1fb28ae54
lionbanana09 <139818521+lionbanana09@users.noreply.github.com>
2025-02-22 22:07:40 +0000

    i

--- src/picoend/please/picow_ble_temp_sensor.py
+++ src/picoend/please/picow_ble_temp_sensor.py
@@ -68,7 +68,7 @@ class BLETemperature:
     def sender(self,type,message,notify=False, indicate=False):
         print(message)
         if type == 0:
-            self._ble.gatts_write(self._handle, struct.pack(f"@h", int(message)))
+            self._ble.gatts_write(self._handle, struct.pack(f"<h", int(message)))
         elif type == 1:
             self._ble.gatts_write(self._handle, struct.pack(f"<{len(message)}s", message))
         for conn_handle in self._connections:
