1671d112e6c1ca418514d2d8b791d1afaf29c5e0
lionbanana09 <139818521+lionbanana09@users.noreply.github.com>
2025-02-22 23:06:21 +0000

    Update picow_ble_temp_reader.py

--- src/picoend/please/picow_ble_temp_reader.py
+++ src/picoend/please/picow_ble_temp_reader.py
@@ -227,6 +227,7 @@ class BLETemperatureCentral:
                     self.size = str(self.size)[1:len(str(self.size))] 
                     print(self.size)
                 self.size = int(self.size) - 1
+                print(data)
                 self._value = struct.unpack(f"<{(self.size)}s", data)
                 self.receivedSize = 0
         except OSError as error:
