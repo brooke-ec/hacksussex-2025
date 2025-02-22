ce3e00c9dac389e3fe5f3e0f608d23d239e9d681
lionbanana09 <139818521+lionbanana09@users.noreply.github.com>
2025-02-22 18:51:09 +0000

    Update ble_advertising.py

--- src/picoend/please/ble_advertising.py
+++ src/picoend/please/ble_advertising.py
@@ -71,7 +71,7 @@ def decode_name(payload):
 def decode_services(payload):
     services = []
     for u in decode_field(payload, _ADV_TYPE_UUID16_COMPLETE):
-        services.append(bluetooth.UUID(struct.unpack("<s", u)[0]))
+        services.append(bluetooth.UUID(struct.unpack("<h", u)[0]))
     for u in decode_field(payload, _ADV_TYPE_UUID32_COMPLETE):
         services.append(bluetooth.UUID(struct.unpack("<d", u)[0]))
     for u in decode_field(payload, _ADV_TYPE_UUID128_COMPLETE):
