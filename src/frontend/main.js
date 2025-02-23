d0ab579924717ad1f2685020f8c9ed485037fe23
Rosalina <152009650+Rosalina-Chan@users.noreply.github.com>
2025-02-23 03:29:51 +0000

    Update main.js

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -3,5 +3,5 @@ function storeNameandRedirect(name){
         return;
     }
     localStorage.setItem("name", name)
-    window.location.replace("http://127.0.0.1:5500/chat.html")
+    window.location.replace("/chat.html")
 }
