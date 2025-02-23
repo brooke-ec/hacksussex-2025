daeeeb688dc146ff29115e22e3ee08f8681a2c0b
Brooke <46959407+brooke-ec@users.noreply.github.com>
2025-02-23 03:20:26 +0000

    test characteristic

--- src/picoend/aioble-client.py
+++ src/picoend/aioble-client.py
@@ -1,14 +1,28 @@
 import asyncio
+
+from numpy import char
 import bluetooth
 import aioble
 
 _SERVICE_UUID = bluetooth.UUID(0x181A)
-
-_ADV_INTERVAL = const(1000)
+_CHARACTERISTIC_UUID = bluetooth.UUID(0x181A)
+service = aioble.Service(_SERVICE_UUID)
+characteristic = aioble.Characteristic(service, _CHARACTERISTIC_UUID, write=True, notify=True)
+aioble.register_services(service)
 
 async def main():
-    async with aioble.scan(duration_ms=6000) as scanner:
+    async with aioble.scan(5000, 30000, 30000, active=True) as scanner:
         async for result in scanner:
-            print(result, result.name(), result.rssi, result.services())
+            if _SERVICE_UUID in result.services():
+                device = result.device
+                break
+        else:
+            print("File server not found")
+            return
+        
+        print(f"Found {device}")
+        connection = await device.connect()
+        
+        characteristic.notify(connection, "Hello World")
 
 asyncio.run(main())
--- src/picoend/aioble-server.py
+++ src/picoend/aioble-server.py
@@ -3,6 +3,10 @@ import bluetooth
 import aioble
 
 _SERVICE_UUID = bluetooth.UUID(0x181A)
+_CHARACTERISTIC_UUID = bluetooth.UUID(0x181A)
+service = aioble.Service(_SERVICE_UUID)
+characteristic = aioble.Characteristic(service, _CHARACTERISTIC_UUID, write=True, notify=True)
+aioble.register_services(service)
 
 _ADV_INTERVAL = const(1000)
 
@@ -14,5 +18,10 @@ async def main():
         )
     
     print(f"Connection From {connection.device}")
+
+    await characteristic.written()
+    msg = characteristic.read()
+
+    print(f"Recieved: {msg}")
 
 asyncio.run(main())
