07d07d6d24c1cabe9acdaedceb2856214f01a250
Rosalina <152009650+Rosalina-Chan@users.noreply.github.com>
2025-02-23 07:14:47 +0000

    Update chat.js

--- src/frontend/chat.js
+++ src/frontend/chat.js
@@ -2,16 +2,19 @@ function sendNewMessage(name, message){
     if (message == ""){
         return;
     }
-    //Call Python code here
+    //window.pywebview.api.funcName(name, message)
     document.getElementById("message").value = "";
     addNewMessage(name, message)
 }
 function addNewMessage(name, message){
+    colour = name.toString(16);
+    console.log(colour);
     let temp = document.getElementsByTagName("template")[0];
     let clon = temp.content.cloneNode(true);
     clon.querySelector("h3").innerText = name;
     clon.querySelector(".messageTextBox").innerText = message;
-    document.querySelector(".chatBorder").appendChild(clon)
+    clon.querySelector(".userIconThing").style.background = colour;
+    document.querySelector(".chatBorder").appendChild(clon);
     window.scrollTo(0, document.body.scrollHeight);
 }
 window.addEventListener("load", function(){
