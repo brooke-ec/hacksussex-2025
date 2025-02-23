440b49d38316f18352ed21c5cfebd2f34cb0fecd
Jaiden HT <jaiden.hewitt.taylor@gmail.com>
2025-02-23 04:03:02 +0000

    adding backend and pico component stuff :3
    adds backend stuff (app.py), notification thingy and LCD test thing :clock4:

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,5 +1,25 @@
 from api import Api
 import webview
+#import cryptography # type: ignore
+from os import path
 
-window = webview.create_window('Woah dude!', 'https://pywebview.flowrl.com', js_api=Api())
-webview.start(debug=True)
+
+def handler():
+  print(f'There are {len(webview.windows)} windows')
+  print(f'Active window: {webview.active_window()}')
+
+def logic():
+   #window.evaluate_js('C:/Users/jh2046/Documents/GitHub/hacksussex-2025/src/frontend/main.js')
+   print("aaa")
+
+def on_closing():
+    print("Such devastation... this was NOT my intention")
+
+
+#print(os.getcwd())
+window = webview.create_window('test', '../frontend/index.html')
+window.events.closing += on_closing
+webview.start(logic)
+
+
+pass
